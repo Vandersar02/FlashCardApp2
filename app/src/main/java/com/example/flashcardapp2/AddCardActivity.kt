d9757333213fbf77ab43ea6c1bcf9cb87f7ffd61
Vandersa02 <leejvan02@gmail.com>
2024-03-31 16:37:06 -0400

    first commit

--- app/src/main/java/com/example/flashcardapp2/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp2/AddCardActivity.kt
@@ -1,88 +1,85 @@
 package com.example.flashcardapp2
 
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 
 class AddCardActivity : AppCompatActivity() {
 
-    private val ADD_CARD_REQUEST = 1
+    private lateinit var flashcardDatabase: FlashcardDatabase
+    private var isUpdate: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_add_card)
 
-        // Récupération des données de l'Intent
-        val question = intent.getStringExtra("question")
-        val answer1 = intent.getStringExtra("answer1")
-        val answer2 = intent.getStringExtra("answer2")
-        val answer3 = intent.getStringExtra("answer3")
+        flashcardDatabase = FlashcardDatabase(this)
 
+        val btnSave = findViewById<View>(R.id.saveBtn)
+        val btnClose = findViewById<View>(R.id.closeBtn)
 
-        val questionText = findViewById<TextView>(R.id.editQuestion)
-        val answerText1 = findViewById<TextView>(R.id.editAnswer1)
-        val answerText2 = findViewById<TextView>(R.id.editAnswer2)
-        val answerText3 = findViewById<TextView>(R.id.editAnswer3)
-
-        val closeBtn = findViewById<ImageView>(R.id.closeBtn)
-        val saveBtn = findViewById<ImageView>(R.id.saveBtn)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        isUpdate = intent.getBooleanExtra("isUpdate", false)
+        if (isUpdate) {
+            // Si c'est une mise à jour, remplir les champs avec les données de la carte actuelle
+            fillFieldsWithFlashcardData()
         }
 
-        saveBtn.setOnClickListener {
-            // Retrieve user input
-            val question = questionText.text.toString()
-            val answer1 = answerText1.text.toString()
-            val answer2 = answerText2.text.toString()
-            val answer3 = answerText3.text.toString()
-
-            val toast = Toast.makeText(this, "field all the line !!!", Toast.LENGTH_SHORT);
-
-            if (answer1.isEmpty() || answer2.isEmpty() ||answer3.isEmpty() || question.isEmpty()) {
-                // Show a toast message if one of them
-                toast.show()
-            }
-            else {
-                // Create an Intent to pass data back to MainActivity
-                val resultIntent = Intent().apply {
-                    putExtra("question", question)
-                    putExtra("answer1", answer1)
-                    putExtra("answer2", answer2)
-                    putExtra("answer3", answer3)
-
-                }
-                setResult(Activity.RESULT_OK, resultIntent)
-
-                // Finish AddCardActivity
-                finish()
-            }
+        btnSave.setOnClickListener {
+            saveFlashcard()
         }
 
-
-        closeBtn.setOnClickListener {
-            // Finish AddCardActivity
+        btnClose.setOnClickListener {
             finish()
         }
-
-        questionText.setText(question)
-        answerText1.setText(answer1)
-        answerText2.setText(answer2)
-        answerText3.setText(answer3)
-
     }
 
+    private fun fillFieldsWithFlashcardData() {
+        val question = intent.getStringExtra("question")
+        val answer = intent.getStringExtra("answer1")
+        val wrongAnswer1 = intent.getStringExtra("answer2")
+        val wrongAnswer2 = intent.getStringExtra("answer3")
 
+        findViewById<EditText>(R.id.editQuestion).setText(question)
+        findViewById<EditText>(R.id.editAnswer1).setText(answer)
+        findViewById<EditText>(R.id.editAnswer2).setText(wrongAnswer1)
+        findViewById<EditText>(R.id.editAnswer3).setText(wrongAnswer2)
+    }
+
+    private fun saveFlashcard() {
+        val editQuestion = findViewById<EditText>(R.id.editQuestion).text.toString()
+        val editAnswer1 = findViewById<EditText>(R.id.editAnswer1).text.toString()
+        val editAnswer2 = findViewById<EditText>(R.id.editAnswer2).text.toString()
+        val editAnswer3 = findViewById<EditText>(R.id.editAnswer3).text.toString()
+        val uuid = intent.getIntExtra("uuid", -1)
+
+        if (editQuestion.isEmpty() || editAnswer1.isEmpty() || editAnswer2.isEmpty() || editAnswer3.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val newFlashcard = Flashcard(editQuestion, editAnswer1, editAnswer2, editAnswer3)
+
+        if (isUpdate && uuid != -1) {
+            // Mise à jour de la carte existante
+            newFlashcard.uuid = uuid
+            flashcardDatabase.updateCard(newFlashcard)
+            Toast.makeText(this, "Successfully Update.", Toast.LENGTH_SHORT).show()
+        } else {
+            // Ajout d' une nouvelle carte
+            flashcardDatabase.insertCard(newFlashcard)
+            Toast.makeText(this, "Successfully Create.", Toast.LENGTH_SHORT).show()
+        }
+        // Retourner le résultat à l'activité appelante
+        setResult(Activity.RESULT_OK)
+
+        // Redémarrer MainActivity
+        startActivity(Intent(this, MainActivity::class.java))
+
+        finish()
+    }
 }
--- app/src/main/java/com/example/flashcardapp2/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp2/MainActivity.kt
@@ -1,74 +1,181 @@
 package com.example.flashcardapp2
 
-import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
 
-    private val ADD_CARD_REQUEST = 1
+class MainActivity : AppCompatActivity() {
+    private val addRequest = 1
+    private val editRequest = 2
+    private var currentFlashcardIndex = 0
+    private var allFlashcards = mutableListOf<Flashcard>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialisez votre base de données et récupérez les flashcards
+        val flashcardDatabase = FlashcardDatabase(this)
+        flashcardDatabase.initFirstCard()
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+        // afficher tous les élements de la base de données
+        displayFlashcard(currentFlashcardIndex)
+
+        val parentLayout = findViewById<View>(R.id.parentLayout)
+
         val questionText = findViewById<TextView>(R.id.question_txt)
         val answerText1 = findViewById<TextView>(R.id.answer1_txt)
         val answerText2 = findViewById<TextView>(R.id.answer2_txt)
         val answerText3 = findViewById<TextView>(R.id.answer3_txt)
+
         val plusBtn = findViewById<View>(R.id.plusBtn)
         val editBtn = findViewById<View>(R.id.editBtn)
+        val btnNext = findViewById<View>(R.id.btnNext)
+        val btnDelete = findViewById<View>(R.id.btnDelete)
+
+        answerText1.setOnClickListener {
+            if((answerText1.text).isNotEmpty()){
+                answerText1.setBackgroundColor(Color.GREEN)
+                answerText2.isEnabled = false
+                answerText3.isEnabled = false
+            }
+
+        }
+
+        answerText2.setOnClickListener {
+            if((answerText2.text).isNotEmpty()){
+                answerText1.setBackgroundColor(Color.GREEN)
+                answerText2.setBackgroundColor(Color.RED)
+                answerText1.isEnabled = false
+                answerText3.isEnabled = false
+            }
+
+        }
+
+        answerText3.setOnClickListener {
+            if((answerText3.text).isNotEmpty()){
+                answerText1.setBackgroundColor(Color.GREEN)
+                answerText3.setBackgroundColor(Color.RED)
+                answerText1.isEnabled = false
+                answerText2.isEnabled = false
+            }
+
+        }
+
+        parentLayout.setOnClickListener(View.OnClickListener { // Reset colors of the TextViews
+            answerText1.setBackgroundColor(getResources().getColor(R.color.grown))
+            answerText2.setBackgroundColor(getResources().getColor(R.color.grown))
+            answerText3.setBackgroundColor(getResources().getColor(R.color.grown))
+            answerText1.isEnabled = true
+            answerText2.isEnabled = true
+            answerText3.isEnabled = true
+        })
 
         plusBtn.setOnClickListener {
-            val intent = Intent(this, AddCardActivity::class.java)
-            startActivityForResult(intent, ADD_CARD_REQUEST)
+            val intent = Intent(this@MainActivity, AddCardActivity::class.java)
+            startActivityForResult(intent, addRequest)
         }
 
         editBtn.setOnClickListener {
-            val question = questionText.text.toString()
-            val answer1 = answerText1.text.toString()
-            val answer2 = answerText2.text.toString()
-            val answer3 = answerText3.text.toString()
+            if (allFlashcards.isNotEmpty()) {
+                val (question, answer, wrongAnswer1, wrongAnswer2, uuid) = allFlashcards[currentFlashcardIndex]
 
-            val intent = Intent(this, AddCardActivity::class.java)
-            intent.putExtra("question", question)
-            intent.putExtra("answer1", answer1)
-            intent.putExtra("answer2", answer2)
-            intent.putExtra("answer3", answer3)
-            startActivityForResult(intent, ADD_CARD_REQUEST)
+                val intent = Intent(this@MainActivity, AddCardActivity::class.java)
+                intent.putExtra("isUpdate", true)
+                intent.putExtra("uuid", uuid) // Passer l'UUID de la carte à éditer
+                intent.putExtra("question", question)
+                intent.putExtra("answer1", answer)
+                intent.putExtra("answer2", wrongAnswer1)
+                intent.putExtra("answer3", wrongAnswer2)
+                startActivityForResult(intent, editRequest)
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "No card to edit.", Snackbar.LENGTH_LONG).show()
+            }
         }
 
+        btnNext.setOnClickListener {
+            if (allFlashcards.isEmpty()) {
+                // return here, so that the rest of the code in this onClickListener doesn't execute
+                return@setOnClickListener
+            }
+            // Afficher la carte suivante
+            currentFlashcardIndex =getRandomNumber(maxNumber = allFlashcards.size-1)
+            Log.i("MainActivity","Current index: $currentFlashcardIndex")
+
+            if (currentFlashcardIndex >= allFlashcards.size) {
+                currentFlashcardIndex = 0 // Revenir à la première carte si nous avons atteint la fin
+                Snackbar.make(findViewById(android.R.id.content), "Back to first question.", Snackbar.LENGTH_LONG).show()
+            }
+            if (currentFlashcardIndex == allFlashcards.size-1 && allFlashcards.size == 1){
+                Snackbar.make(findViewById(android.R.id.content), "No more questions.", Snackbar.LENGTH_LONG).show()
+            }
+            displayFlashcard(currentFlashcardIndex)
+        }
+
+        btnDelete.setOnClickListener {
+            if (allFlashcards.isEmpty()) {
+                // return here, so that the rest of the code in this onClickListener doesn't execute
+                return@setOnClickListener
+            }
+
+            val (question) = allFlashcards[currentFlashcardIndex]
+
+            // Supprimer la carte actuelle
+            if (allFlashcards.size > 1) {
+                flashcardDatabase.deleteCard(question)
+                allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+
+                if (currentFlashcardIndex >= allFlashcards.size) {
+                    currentFlashcardIndex = 0 // Revenir à la première carte si nous avons supprimé la dernière
+                }
+
+                displayFlashcard(currentFlashcardIndex)
+            }
+            else if(allFlashcards.size == 1){
+                flashcardDatabase.deleteCard(question)
+                allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+                questionText.text = ""
+                answerText1.text = ""
+                answerText2.text = ""
+                answerText3.text = ""
+                displayFlashcard(currentFlashcardIndex)
+            }
+            else{
+                displayFlashcard(currentFlashcardIndex)
+            }
+
+        }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun displayFlashcard(index: Int) {
+        if (allFlashcards.isNotEmpty()) {
+            val (question, answer, wrongAnswer1, wrongAnswer2) = allFlashcards[index]
 
-        if (requestCode == ADD_CARD_REQUEST && resultCode == Activity.RESULT_OK) {
-            val question = data?.getStringExtra("question")
-            val answer1 = data?.getStringExtra("answer1")
-            val answer2 = data?.getStringExtra("answer2")
-            val answer3 = data?.getStringExtra("answer3")
+            val questionText = findViewById<TextView>(R.id.question_txt)
+            val answerText1 = findViewById<TextView>(R.id.answer1_txt)
+            val answerText2 = findViewById<TextView>(R.id.answer2_txt)
+            val answerText3 = findViewById<TextView>(R.id.answer3_txt)
 
-            // Update the TextViews with the received data
-            findViewById<TextView>(R.id.question_txt).text = question
-            findViewById<TextView>(R.id.answer1_txt).text = answer1
-            findViewById<TextView>(R.id.answer2_txt).text = answer2
-            findViewById<TextView>(R.id.answer3_txt).text = answer3
-
-
-            // Get the root view of the layout
-            val rootView = findViewById<View>(android.R.id.content)
-
-            // Show the snackbar message for the new card
-            Snackbar.make(rootView, "The new card is created successfully", Snackbar.LENGTH_SHORT).show()
-
-
+            questionText.text = question
+            answerText1.text = answer
+            answerText2.text = wrongAnswer1
+            answerText3.text = wrongAnswer2
+        } else {
+            // Afficher un Snackbar indiquant que la base de données est vide
+            Snackbar.make(findViewById(android.R.id.content), "Database is empty", Snackbar.LENGTH_LONG).show()
         }
+    }
+
+    private fun getRandomNumber(minNumber: Int =0, maxNumber: Int): Int {
+        return (minNumber..maxNumber).random()
     }
 
 }
